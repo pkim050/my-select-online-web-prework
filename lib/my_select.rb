d91fd985a0828e776c33ab73ab27834b756cf679
Patrick Kim <pkim050@gmail.com>
2019-02-05 18:48:58 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
  # your code here!
+ counter = 0
+ ary = []
+ ary2 = []
+ 
+ while counter < collection.size
+  ary << yield(collection[counter])
+  if ary[counter] == "true"
+    ary2 << collection[counter]
+  end
+  counter += 1
+  end
+  ary2
 end
